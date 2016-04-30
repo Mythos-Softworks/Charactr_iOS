6d3da24d4f0601b87ec4211136ed28c29f831a66
Jaysen Spurlock <jayspur11@gmail.com>
2016-04-30 10:46:19 -0700

    Created cell for Virtues. Still needs to handle info, interaction, and layout...

---
+++ Charactr/Presentation/Views/CharacterSheets/Scion/Cells/MSScionVirtuesCell.h
@@ -0,0 +1,13 @@
+//
+//  MSScionVirtuesCell.h
+//  Charactr
+//
+//  Created by Jaysen Spurlock on 4/30/16.
+//  Copyright © 2016 Jaysen Spurlock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSScionVirtuesCell : UITableViewCell
+
+@end
---
+++ Charactr/Presentation/Views/CharacterSheets/Scion/Cells/MSScionVirtuesCell.m
@@ -0,0 +1,24 @@
+//
+//  MSScionVirtuesCell.m
+//  Charactr
+//
+//  Created by Jaysen Spurlock on 4/30/16.
+//  Copyright © 2016 Jaysen Spurlock. All rights reserved.
+//
+
+#import "MSScionVirtuesCell.h"
+
+@implementation MSScionVirtuesCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
